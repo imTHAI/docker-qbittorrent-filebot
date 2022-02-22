3cb2815b6bfdfcd396b860a2da838a5363055146
imTHAI <36070606+imTHAI@users.noreply.github.com>
2022-02-22 07:01:28 +0100

    Create fb.sh

---
+++ root/src/fb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Future feature
+#if [ ! -f /filebot/osdb.loggedin ]; then
+# printf "$OSDB_USER\n$OSDB_PASSWD\n" | /filebot/filebot.sh -script fn:configure
+# touch /filebot/osdb.loggedin
+#fi
+
+/filebot/filebot.sh \
+-script fn:amc \
+--output /media \
+--lang "$FILEBOT_LANG" \
+--action "$FILEBOT_ACTION" \
+--conflict "$FILEBOT_CONFLICT" \
+--log-file /data/filebot/filebot.log \
+--def excludeList=/data/filebot/amc-exlude-list.txt \
+-non-strict \
+--def \
+unsorted=y \
+music=/media/"$FILEBOT_PROCESS_MUSIC" \
+musicFormat=/media/"$MUSIC_FORMAT" \
+artwork=/media/"$FILEBOT_ARTWORK" \
+movieFormat=/media/"$MOVIE_FORMAT" \
+seriesFormat=/media/"$SERIE_FORMAT" \
+animeFormat=/media/"$ANIME_FORMAT" \
+ut_dir="$1" \
+ut_title="$2" \
+ut_label="$3"
